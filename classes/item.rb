835d2dbb32fd13a55a0e7909b6ba85c877fab695
MHNazary <mhnazary2020@gmail.com>
2023-10-17 15:45:23 +0430

    Methods added for the item class

--- classes/item.rb
+++ classes/item.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Item
   attr_accessor :author, :label, :genre, :published_date
   attr_reader :archived
@@ -10,5 +12,15 @@ class Item
     @label = nil
     @published_date = Date.parse(published_date)
     @archived = archived
+  end
+
+  # should change the archived property to true
+  # if the result of the can_be_archived?() method is true
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+  # return true if published_date is older than 10 years.
+  def can_be_archived?
+    (Date.today.year - @published_date.year).to_i > 10
   end
 end

095f28466637f80f48514759596d7484b3e3241a
SnakyMz <Soban.syeed@outlook.com>
2023-10-17 23:01:21 +0530

    Add can_be_archived? mehod to Game class

--- classes/game.rb
+++ classes/game.rb
@@ -1,7 +1,9 @@
 require_relative 'item'
-require 'Date'
+require 'date'
 
 class Game < Item
+  attr_accessor :multiplayer, :last_played_at, :published_date
+
   def initialize(multiplayer, last_played_at, published_date)
     super(published_date)
     @multiplayer = multiplayer
@@ -10,6 +12,6 @@ class Game < Item
 
   private
   def can_be_archived?
-    super && (Date.today.year - @last_played_at.year).to_i > 2
+    super && ((Date.today.year - @last_played_at.year).to_i > 2)
   end
 end

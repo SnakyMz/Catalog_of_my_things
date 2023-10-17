66a7cee9f299a86580c67a4608d0892a0534d66c
MHNazary <mhnazary2020@gmail.com>
2023-10-17 22:40:05 +0430

    Methods added for the label class

--- classes/label.rb
+++ classes/label.rb
@@ -7,4 +7,22 @@ class Label
     @color = color
     @items = []
   end
+  
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
+
+  # class in hash form
+  def serialize
+    {
+      'title' => @title,
+      'color' => @color
+    }.to_json
+  end
+
+  # convert hash to class
+  def self.de_serialize(data)
+    new(data['title'], data['color'])
+  end
 end

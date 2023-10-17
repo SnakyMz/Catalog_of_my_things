8d1f184050ed185af037f05a4d3fc3a69b1cc65e
SnakyMz <Soban.syeed@outlook.com>
2023-10-17 21:07:30 +0530

    Fix linters

--- classes/author.rb
+++ classes/author.rb
@@ -1,10 +1,9 @@
-# Class containing data about author
 class Author
   attr_accessor :first_name, :last_name
   attr_reader :items
 
   def initialize(first_name, last_name)
-    @id = rand(1..100)
+    @id = rand(1...100)
     @first_name = first_name
     @last_name = last_name
     @items = []

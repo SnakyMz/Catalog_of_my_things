2126768126228624c77ffe73ad3c64afa5ccfa1c
SnakyMz <Soban.syeed@outlook.com>
2023-10-17 20:58:55 +0530

    Add add_item method to Author class

--- classes/author.rb
+++ classes/author.rb
@@ -1,9 +1,17 @@
 # Class containing data about author
 class Author
+  attr_accessor :first_name, :last_name
+  attr_reader :items
+
   def initialize(first_name, last_name)
     @id = rand(1..100)
     @first_name = first_name
     @last_name = last_name
     @items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
+    item.author = self
   end
 end

d6b622a8c44745655d5dbab320a028bd59273e0a
MHNazary <mhnazary2020@gmail.com>
2023-10-18 20:33:42 +0430

    test case 1 added for the book

---
+++ spec/book_spec.rb
@@ -0,0 +1,14 @@
+require './classes/book'
+
+describe Book do
+  # 2 instance for Book
+  book1 = Book.new('publisher', 'good', '2011-3-11')
+  book2 = Book.new('publisher', 'bad', '2023-2-5')
+
+  # test methods
+  context 'Test for method: can_be_archived' do
+    it 'should return true, parent => true' do
+      expect(book1.can_be_archived?).to be true
+    end
+  end
+end
